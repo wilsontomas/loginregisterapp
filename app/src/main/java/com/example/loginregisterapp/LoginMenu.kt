dc959e7830b3d4a697598b0c11912b9534ae8735
Wilson Tomas <wilsonpumm@gmail.com>
2022-02-12 17:00:49 -0400

    Se creo el proyecto

---
+++ app/src/main/java/com/example/loginregisterapp/LoginMenu.kt
@@ -0,0 +1,11 @@
+package com.example.loginregisterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_menu)
+    }
+}
---
+++ app/src/main/java/com/example/loginregisterapp/RegisterMenu.kt
@@ -0,0 +1,11 @@
+package com.example.loginregisterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_menu)
+    }
+}
